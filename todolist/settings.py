dbe632d2bf885e7c428714cc3082b2874495fc51
anastasiak <anastasiya.koryakina1@gmail.com>
2026-09-18 21:58:22 +0200

    Fix

--- todolist/settings.py
+++ todolist/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
         'USER': os.environ.get('USER'),
         'PASSWORD': os.environ.get('PASSWORD'),
         'HOST': os.environ.get('HOST'),  # You can use a different host if your MySQL server is on a remote machine.
-        'PORT': os.environ.get('PORT'),  # Leave this empty to use the default MySQL port (3306).
+        'PORT': int(os.environ.get('PORT', 3306)),  # Leave this empty to use the default MySQL port (3306).
     }
 }
 
